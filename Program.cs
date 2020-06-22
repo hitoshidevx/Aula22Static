604ce2628ce28e111c32ca8534ba8057abe79686
unknown <g.hitoshi.yokogawa@gmail.com>
2020-06-22 10:02:15 -0300

    Exercício finalizado.

--- Program.cs
+++ Program.cs
@@ -7,10 +7,27 @@ namespace Aula22Static
         static void Main(string[] args)
         {
             
-            Console.WriteLine( Conversor.ConverterDolarParaReal(29.35f) );
-            Console.WriteLine( Conversor.ConverterRealParaDolar(134.57f)+"\n" );
+            //Converte Dólar (moeda americana) para Real (moeda brasileira).
+            Console.WriteLine("Conversão do dólar para real:");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine( Conversor.ConverterDolarParaReal(29.35f)+"\n" );
+            Console.ResetColor();
 
-            Console.WriteLine( Conversor.ConverterEuroParaReal(49.83f) );
+            //Converte Real (moeda brasileira) para Dólar (moeda americana).
+            Console.WriteLine("Conversão do real para dólar:");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine( Conversor.ConverterRealParaDolar(134.57f)+"\n" );
+            Console.ResetColor();
+
+            //Converte Euro (moeda da União Européia) para Real (moeda brasileira).
+            Console.WriteLine("Conversão do euro para real:");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine( Conversor.ConverterEuroParaReal(49.83f)+"\n" );
+            Console.ResetColor();
+
+            //Converte Real (moeda brasileira) para Euro (moeda da União Européia).
+            Console.WriteLine("Conversão do real para euro:");
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
             Console.WriteLine( Conversor.ConverterRealParaEuro(78.25f) );
 
         }
