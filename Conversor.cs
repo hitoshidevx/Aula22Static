c23b14c2d0820c6a0c126543334db9514bf98511
unknown <g.hitoshi.yokogawa@gmail.com>
2020-06-22 09:46:03 -0300

    Estrutura da conversão feita.

--- Conversor.cs
+++ Conversor.cs
@@ -1,7 +1,39 @@
 namespace Aula22Static
 {
-    public class Conversor
+    public static class Conversor
     {
         
+        public static float ValorDolar = 5.29f;
+
+        //Converte Dólar (moeda americana) para Real (moeda brasileira).
+        public static float ConverterDolarParaReal(float valor){
+
+            return valor * ValorDolar;
+
+        }
+        //Converte Real (moeda brasileira) para Dólar (moeda americana).
+        public static float ConverterRealParaDolar(float valor){
+
+            return valor / ValorDolar;
+
+        }
+
+        public static float ValorEuro = 5.90f;
+
+        //Converte Euro (moeda da União Européia) para Real (moeda brasileira).
+        public static float ConverterEuroParaReal(float valor){
+
+            return valor * ValorEuro;
+
+        }
+        //Converte Real (moeda brasileira) para Euro (moeda da União Européia).
+        public static float ConverterRealParaEuro(float valor){
+
+            return valor / ValorEuro;
+
+        }
+
+
+
     }
 }
